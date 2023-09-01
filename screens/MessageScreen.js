5e94213f130d94a95aa634a7503a2c2e82e4f75e
Abdulhamid <mobolaji2309@gmail.com>
2023-09-01 10:09:51 +0100

    updating

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -8,12 +8,23 @@ import {
   FlatList,
   TouchableWithoutFeedback,
   Keyboard,
+  TouchableOpacity,
 } from "react-native";
-import React, { useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import Header from "./components/Header";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import SenderMessage from "./components/SenderMessage";
+import ReceiverMessage from "./components/ReceiverMessage";
+import { FontAwesome } from "@expo/vector-icons";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+} from "@firebase/firestore";
+import db from "../firebase";
 const MessageScreen = () => {
   const navigation = useNavigation();
   useLayoutEffect(() => {
@@ -23,7 +34,32 @@ const MessageScreen = () => {
   });
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
-  const sendMessage = () => {};
+  // useEffect(() => {
+  //   () =>
+  //     onSnapshot(
+  //       query(
+  //         collection(db, "matches", matchDetails.id, "messages"),
+  //         orderBy("timestamp", "desc")
+  //       ),
+  //       (snaphot) =>
+  //         setMessages(
+  //           snapshot.docs.map((doc) => ({
+  //             id: doc.id,
+  //             ...doc.data(),
+  //           }))
+  //         )
+  //     );
+  // }, [matchDetails, db]);
+  const sendMessage = () => {
+    // addDoc(collection(db, "matches", matchDetails.id, "messages"), {
+    //   timestamp: serverTimestamp(),
+    //   userId: user.uid,
+    //   displayName: user.displayName,
+    //   photoURL: matchDetails.users[user.uid].photoURL,
+    //   message: input,
+    // });
+    // setInput("");
+  };
   return (
     <SafeAreaView className="flex-1">
       <Header title="Elon Musk" callEnabled />
@@ -31,11 +67,14 @@ const MessageScreen = () => {
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         className="flex-1"
         keyboardVerticalOffset={10}
-      >
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <FlatList />
-        </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
+      ></KeyboardAvoidingView>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View>
+          <SenderMessage />
+          <ReceiverMessage />
+        </View>
+      </TouchableWithoutFeedback>
+
       <View className="bg-white flex-row justify-between items-center border-t border-gray-200 px-5 py-2">
         <TextInput
           style={{}}
@@ -45,7 +84,11 @@ const MessageScreen = () => {
           onSubmitEditing={sendMessage}
           value={input}
         />
-        <Button onPress={sendMessage} title="Send" color="#FF5864" />
+        {/* <Button onPress={sendMessage} title="Send" color="#FF5864" />
+         */}
+        <TouchableOpacity>
+          <FontAwesome name="send" size={24} color="#FF5864" />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
--- screens/components/SenderMessage.js
+++ screens/components/SenderMessage.js
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const SenderMessage = () => {
+const SenderMessage = ({ message }) => {
   return (
-    <View>
-      <Text>SenderMessage</Text>
+    <View
+      style={{ alignSelf: "flex-start", marginLeft: "auto" }}
+      className="bg-purple-600 rounded-lg rounded-tr-noen px-5 py-3 mx-3 my-2"
+    >
+      <Text className="text-white">Message</Text>
     </View>
   );
 };
